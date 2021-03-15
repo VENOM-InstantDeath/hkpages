8bf2fab1a17017b4e4c6cd79973922e34df2a92c
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-03-14 23:41:28 -0300

    Updated kain

--- priv/kain.py
+++ priv/kain.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
-
+import os
 import subprocess
+
+
+def get(p):
+    x=subprocess.Popen(p+"x", shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return x.stdout.read(), x.stderr.read().decode()
+
+
+print("Ruta del script (no incluye el script)")
 p=input("Input path: ")
-x=subprocess.Popen(path, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-print(f"\n\n\n\nSTDOUT: {x.stdout.read()}\n\nSTDERR: {x.stderr.read().decode()}")
+os.chdir(p)
+if not p.endswith('/'): p+='/'
+x, y = get(p)
+print(f"\n\n\n\nSTDOUT: {x}\n\nSTDERR: {y}")
