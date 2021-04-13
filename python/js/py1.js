912e6b5d4d17ae259186bbc41db1265b31c3501e
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-04-13 16:10:39 -0300

    Updated python dir

---
+++ python/js/py1.js
@@ -0,0 +1,9 @@
+var qone = 0;
+var qtwo = 0;
+
+document.getElementById("beqone").addEventListener("click", function(){document.getElementById("qone").innerHTML = " Correcto"});
+document.getElementById("bsqone").addEventListener("click", function(){document.getElementById("qone").innerHTML = " Incorrecto"});
+document.getElementById("beqtwo").addEventListener("click", function(){document.getElementById("qtwo").innerHTML = " Incorrecto"});
+document.getElementById("bsqtwo").addEventListener("click", function(){document.getElementById("qtwo").innerHTML = " Correcto"});
+document.getElementById("beqthree").addEventListener("click", function(){document.getElementById("qthree").innerHTML = " Incorrecto"});
+document.getElementById("bsqthree").addEventListener("click", function(){document.getElementById("qthree").innerHTML = " Correcto"});
