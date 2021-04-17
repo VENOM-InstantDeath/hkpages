7b3fc01fc726996d23d0786c47e89c25cd955ee8
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-04-17 14:11:21 -0300

    Updated python dir

--- python/js/py1.js
+++ python/js/py1.js
@@ -7,4 +7,5 @@ document.getElementById("beqtwo").addEventListener("click", function(){document.
 document.getElementById("bsqtwo").addEventListener("click", function(){document.getElementById("qtwo").innerHTML = " Correcto"});
 document.getElementById("beqthree").addEventListener("click", function(){document.getElementById("qthree").innerHTML = " Incorrecto"});
 document.getElementById("bsqthree").addEventListener("click", function(){document.getElementById("qthree").innerHTML = " Correcto"});
-document.getElementById("prev").addEventListener("click", window.location.replace('https://google.com'))
+document.getElementById("prev").addEventListener("click", function() {window.location.replace('index.html')});
+document.getElementById("next").addEventListener("click", function() {window.location.replace('py2.html')});
